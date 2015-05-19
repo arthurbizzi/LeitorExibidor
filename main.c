924834f40d4445c0496378afe6da446fdf2b6440
Bruno Ribeiro das Virgens <Grandeaguia@gmail.com>
2015-05-19 15:11:52 -0300

    Commit Inicial
    Arquivos iniciais do projeto

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+*    Trabalho da Disciplina Software Básico, 2015-1
+*    Alunos: Bruno Ribeiro das Virgens (11/0111141)
+*            Kelvin William Moreira Lima (11/0159560)
+*            Guilherme
+*
+*    Compilar com o comando:
+*    gcc main.c -o leitorexibidor.exe
+*
+*    Chamar com o comando:
+*    leitorexibidor.exe
+*/
+
+#include "main.h"
+
+int main() {
+    printf("LEITOR E EXIBIDOR DE ARQUIVO EM FORMATO .CLASS\n");
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Defines */
+
+/* Estruturas */
+
+/* Prototipos */
+
+#endif
