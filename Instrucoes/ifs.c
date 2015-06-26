d3b84514637701b98b237a27eb5f6fbd3d22bf4f
kelvinec <kelvinlima@yahoo.com.br>
2015-06-26 09:37:04 -0300

    Adicionada documentação para o arquivo ifs.h, corrigido erro de tipo para o index2 das instruções.

--- Instrucoes/ifs.c
+++ Instrucoes/ifs.c
@@ -5,49 +5,49 @@
 #include <math.h>
 
 
-void i_ifeq(Frame* frame,u1 index1,u2 index2){
+void i_ifeq(Frame* frame,u1 index1,u1 index2){
     u4 result = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     if(result == 0){
 		frame->pc += (index1<<8)+index2;
     }
 }
 
-void i_ifne(Frame* frame,u1 index1,u2 index2){
+void i_ifne(Frame* frame,u1 index1,u1 index2){
     u4 result = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     if(result != 0){
 		frame->pc += (index1<<8)+index2;
     }
 }
 
-void i_iflt(Frame* frame,u1 index1,u2 index2){
+void i_iflt(Frame* frame,u1 index1,u1 index2){
     u4 result = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     if(result < 0){
 		frame->pc += (index1<<8)+index2;
     }
 }
 
-void i_ifge(Frame* frame,u1 index1,u2 index2){
+void i_ifge(Frame* frame,u1 index1,u1 index2){
     u4 result = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     if(result >= 0){
 		frame->pc += (index1<<8)+index2;
     }
 }
 
-void i_ifgt(Frame* frame,u1 index1,u2 index2){
+void i_ifgt(Frame* frame,u1 index1,u1 index2){
     u4 result = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     if(result > 0){
 		frame->pc += (index1<<8)+index2;
     }
 }
 
-void i_ifle(Frame* frame,u1 index1,u2 index2){
+void i_ifle(Frame* frame,u1 index1,u1 index2){
     u4 result = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     if(result <= 0){
 		frame->pc += (index1<<8)+index2;
     }
 }
 
-void i_if_icmpeq(Frame* frame,u1 index1,u2 index2){
+void i_if_icmpeq(Frame* frame,u1 index1,u1 index2){
     u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -56,7 +56,7 @@ void i_if_icmpeq(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_icmpne(Frame* frame,u1 index1,u2 index2){
+void i_if_icmpne(Frame* frame,u1 index1,u1 index2){
 	u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -65,7 +65,7 @@ void i_if_icmpne(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_icmplt(Frame* frame,u1 index1,u2 index2){
+void i_if_icmplt(Frame* frame,u1 index1,u1 index2){
     u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -74,7 +74,7 @@ void i_if_icmplt(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_icmpge(Frame* frame,u1 index1,u2 index2){
+void i_if_icmpge(Frame* frame,u1 index1,u1 index2){
     u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -83,7 +83,7 @@ void i_if_icmpge(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_icmpgt(Frame* frame,u1 index1,u2 index2){
+void i_if_icmpgt(Frame* frame,u1 index1,u1 index2){
     u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -92,7 +92,7 @@ void i_if_icmpgt(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_icmple(Frame* frame,u1 index1,u2 index2){
+void i_if_icmple(Frame* frame,u1 index1,u1 index2){
      u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -101,7 +101,7 @@ void i_if_icmple(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_acmpeq(Frame* frame,u1 index1,u2 index2){
+void i_if_acmpeq(Frame* frame,u1 index1,u1 index2){
     u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
@@ -110,7 +110,7 @@ void i_if_acmpeq(Frame* frame,u1 index1,u2 index2){
     }
 }
 
-void i_if_acmpne(Frame* frame,u1 index1,u2 index2){
+void i_if_acmpne(Frame* frame,u1 index1,u1 index2){
 	u4 value2 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
     u4 value1 = DesempilhaOperando32bits(&(frame->pilhaDeOperandos));
 
--- Instrucoes/ifs.h
+++ Instrucoes/ifs.h
@@ -1,19 +1,152 @@
+/**
+* @file ifs.h
+* @author Kelvin
+* @brief Arquivo com definição de protótipos pras funções que implementam as instruções de jumps condicionais.
+*/
+
+
 #include "../Pilha/pilha_operandos.h"
 #include "../Pilha/pilha_frames.h"
 #include "../ClassLoader/classloader.h"
 #include <math.h>
 
-void i_ifeq(Frame* frame,u1 index1,u2 index2);
-void i_ifne(Frame* frame,u1 index1,u2 index2);
-void i_iflt(Frame* frame,u1 index1,u2 index2);
-void i_ifge(Frame* frame,u1 index1,u2 index2);
-void i_ifgt(Frame* frame,u1 index1,u2 index2);
-void i_ifle(Frame* frame,u1 index1,u2 index2);
-void i_if_icmpeq(Frame* frame,u1 index1,u2 index2);
-void i_if_icmpne(Frame* frame,u1 index1,u2 index2);
-void i_if_icmplt(Frame* frame,u1 index1,u2 index2);
-void i_if_icmpge(Frame* frame,u1 index1,u2 index2);
-void i_if_icmpgt(Frame* frame,u1 index1,u2 index2);
-void i_if_icmple(Frame* frame,u1 index1,u2 index2);
-void i_if_acmpeq(Frame* frame,u1 index1,u2 index2);
-void i_if_acmpne(Frame* frame,u1 index1,u2 index2);
+
+/**
+*   @fn void i_ifeq(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha um valor, se igual a zero, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_ifeq(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_ifne(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha um valor, se diferente de zero, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_ifne(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_iflt(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha um valor, se menor que zero, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_iflt(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_ifge(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha um valor, se maior ou igual a zero, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_ifge(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_ifgt(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha um valor, se maior que zero, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_ifgt(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_ifle(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha um valor, se menor ou igual a zero, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_ifle(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_icmpeq(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha dois inteiros, se forem iguais, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_icmpeq(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_icmpne(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha dois inteiros, se não forem iguais, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_icmpne(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_icmplt(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha dois inteiros, se o valor 1 for menor que o valor2, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_icmplt(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_icmpge(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha dois inteiros, se o valor 1 for maior ou igual ao valor2, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_icmpge(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_icmpgt(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha dois inteiros, se o valor 1 for maior que valor2, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_icmpgt(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_icmple(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha dois inteiros, se o valor 1 for menor ou igual ao valor2, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_icmple(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_acmpeq(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha duas referencias para objetos, se as referências forem iguais, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_acmpeq(Frame* frame,u1 index1,u1 index2);
+
+/**
+*   @fn void i_if_acmpne(Frame* frame, u1 index1, u1 index2)
+*   @brief Função que desempilha duas referencias para objetos, se as referências não forem iguais, desvia para a instrução no branchoffset(que é um short com sinal construido a partir dos bytes sem sinal: index1 << 8 + index2)
+*   @param frame Frame corrente.
+*   @param index1 Primeiros 8bits do branchoffset.
+*   @param index2 Últimos 8bits do branchoffset.
+*   @return
+*/
+void i_if_acmpne(Frame* frame,u1 index1,u1 index2);
