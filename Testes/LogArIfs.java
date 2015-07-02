312169b3819ab2575415e2b84ca2eb5f2b788ae9
kelvinec <kelvinlima@yahoo.com.br>
2015-07-02 07:52:18 -0300

    Teste Adicionado

---
+++ Testes/LogArIfs.java
@@ -0,0 +1,15 @@
+class HelloWorldApp {
+    public static void main(String[] args) {
+        double teste1 = -0.3828342d;
+        float teste2 = 0;
+        long teste3 = 255;
+        	while(teste1<5){
+        	if(teste1 < 0){
+        		teste2 = 10;
+        		teste1 = 5;
+        	}else if(teste1>0){
+        		teste3 = teste3 << 16 | 0xFF;
+        	}
+        }
+    }
+}
